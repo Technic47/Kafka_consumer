1d6032568b8c7c2475b92ad022eb655318451c53
Pavel Kuznetsov <Technic47@gmail.com>
2025-09-26 14:09:37 +0300

    DB set up is done.

---
+++ src/main/java/ru/kuznetsov/kafka_consumer/config/SpringConfig.java
@@ -0,0 +1,31 @@
+package ru.kuznetsov.kafka_consumer.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SpringConfig {
+
+    @Value("${spring.datasource.driverClassName}")
+    private String driver;
+    @Value("${spring.datasource.url}")
+    private String jdbcURL;
+    @Value("${spring.datasource.username}")
+    private String username;
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(driver);
+        dataSource.setUrl(jdbcURL);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
+}
