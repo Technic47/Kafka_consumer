f8fe0a865214b89a5b8bba36e737fabd4c7ce58d
Pavel Kuznetsov <Technic47@gmail.com>
2025-09-30 23:13:09 +0300

    Generation is removed.

--- src/main/java/ru/kuznetsov/kafka_consumer/listener/MessageListener.java
+++ src/main/java/ru/kuznetsov/kafka_consumer/listener/MessageListener.java
@@ -2,6 +2,8 @@ package ru.kuznetsov.kafka_consumer.listener;
 
 
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 import ru.kuznetsov.kafka_consumer.model.CommentEntity;
@@ -18,8 +20,11 @@ public class MessageListener {
 
     private final CommentService commentService;
 
+    Logger logger = LoggerFactory.getLogger(MessageListener.class);
+
     @KafkaListener(topics = MESSAGE_TOPIC_NAME, groupId = GROUP_ID)
     public void listenGroupFoo(String message) {
+        logger.info("Received message: {}", message);
         commentService.save(new CommentEntity(message, GROUP_ID, new Date()));
     }
 }
--- src/main/java/ru/kuznetsov/kafka_consumer/service/CommentService.java
+++ src/main/java/ru/kuznetsov/kafka_consumer/service/CommentService.java
@@ -1,7 +1,10 @@
 package ru.kuznetsov.kafka_consumer.service;
 
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import ru.kuznetsov.kafka_consumer.listener.MessageListener;
 import ru.kuznetsov.kafka_consumer.model.CommentEntity;
 import ru.kuznetsov.kafka_consumer.repository.CommentRepository;
 
@@ -10,7 +13,13 @@ import ru.kuznetsov.kafka_consumer.repository.CommentRepository;
 public class CommentService {
     private final CommentRepository commentRepository;
 
+    Logger logger = LoggerFactory.getLogger(MessageListener.class);
+
     public void save(CommentEntity comment) {
+        logger.info("Save comment");
+
         commentRepository.save(comment);
+
+        logger.info("Comment saved");
     }
 }
